38fc3f03b8e35f640db61d2a73c434c369de1628
Luizangel50 <luiz.angel.100@gmail.com>
2015-06-30 17:13:02 -0300

    Fourth Commit
    Visualização do nome do usuário que digitou a mensagem

--- node/js/chat-script.js
+++ node/js/chat-script.js
@@ -1,17 +1,31 @@
 var socket = io();
-var d = $('#chatArea');     
-var m_ImputMessage = $('#inputMessage');
-var m_usernameImput = $('#usernameInput');
+var d = $('#chatArea');
+var m = [$('#usernameInput'), $('#inputMessage')];
+var n = $('#usernameInput');
+// var $usernameDiv = $('<span class="username"/>')
+//       .text(m[0].val())
+//       .css('color', 'blue');
+//     var $messageBodyDiv = $('<span class="messageBody">')
+//       .text(m[1].val());
+//n.style.fontWeight = bold;
+//m[0].val().style.fontWeight = "bold";
+// m.InputMessage = $('#inputMessage');
+// m.usernameInput = $('#usernameInput');
 
 $('form').submit(function(){
-  socket.emit('new message', m_usernameImput.val() + " said: " + m_ImputMessage.val());
-  m_ImputMessage.val('');
-  return false;
+	if (m[1].val() != ""){
+	socket.emit('new message', n.val() + " said: " + m[1].val());
+	}
+  	else{
+  		socket.emit('new message', "");
+  		}
+  	m[1].val('');
+  	return false;
 });
 
 socket.on('new message', function(data){
   if(data != ""){
-    $('#chatArea').append($('<li>').text(data));
+    $('#chatArea').append($('<li>').append(data));
     d.scrollTop(d[0].scrollHeight);
   }
 });
